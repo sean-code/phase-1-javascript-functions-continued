04dd951f7f9535dcc9c926ddcd0e9af2ce301fe0
sean-code <john.nganga@student.moringaschool.com>
2022-08-16 19:35:09 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity1 = "roller-skate"){
+    return (`This Saturday, I want to ${activity1}!`)
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bath my dog"));
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+console.log(mondayWork());
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+        return `You are ${style}${adjective}${style}!`;
+
+    }
+    
+  }
